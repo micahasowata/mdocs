7da34f48e82de5347223d01a3112ca9308c2ced6
Micah Asowata <andrewgary963@gmail.com>
2024-04-15 16:08:41 +0100

    feat: add new docs routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Index from "./pages/Index";
 import AuthRoutes from "./pages/docs/AuthRoutes";
 import ErrorPage from "./pages/docs/ErrorPage";
 import Intro from "./pages/docs/Intro";
+import PostsRoutes from "./pages/docs/PostsRoutes";
+import UsersRoutes from "./pages/docs/UsersRoutes";
 
 const App = () => {
 	const helmetContext = {};
@@ -25,6 +27,14 @@ const App = () => {
 				{
 					path: "/docs/auth",
 					element: <AuthRoutes />,
+				},
+				{
+					path: "/docs/users",
+					element: <UsersRoutes />,
+				},
+				{
+					path: "/docs/posts",
+					element: <PostsRoutes />,
 				},
 			],
 		},
