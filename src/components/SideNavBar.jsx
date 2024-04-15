c43039b572c37f5b171425089a46fcee6956bf78
Micah Asowata <andrewgary963@gmail.com>
2024-04-15 16:08:02 +0100

    fix: correct the active link overflow

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -5,22 +5,39 @@ const SideNavBar = () => {
 			<div>
 				<ul className="flex flex-col scroll-smooth">
 					<NavLink
-						exact
 						to={"/docs"}
 						className={({ isActive }) =>
 							isActive ? "font-bold" : "font-medium"
 						}
+						end
 					>
 						Introduction
 					</NavLink>
+
+					<h2>API Reference</h2>
 					<NavLink
-						exact
 						to={"/docs/auth"}
 						className={({ isActive }) =>
 							isActive ? "font-bold" : "font-medium"
 						}
 					>
 						Auth
+					</NavLink>
+					<NavLink
+						to={"/docs/users"}
+						className={({ isActive }) =>
+							isActive ? "font-bold" : "font-medium"
+						}
+					>
+						Users
+					</NavLink>
+					<NavLink
+						to={"/docs/posts"}
+						className={({ isActive }) =>
+							isActive ? "font-bold" : "font-medium"
+						}
+					>
+						Post
 					</NavLink>
 				</ul>
 			</div>
