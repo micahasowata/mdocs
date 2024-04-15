6f194e694ef3eb9a793cc938071cd48f3b107ea9
Micah Asowata <andrewgary963@gmail.com>
2024-04-15 15:22:53 +0100

    refactor: update app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Main from "./layout/Main";
-import ErrorPage from "./pages/ErrorPage";
+import Docs from "./layout/Docs";
 import Index from "./pages/Index";
+import AuthRoutes from "./pages/docs/AuthRoutes";
+import ErrorPage from "./pages/docs/ErrorPage";
+import Intro from "./pages/docs/Intro";
 
 const App = () => {
 	const helmetContext = {};
 	const router = createBrowserRouter([
 		{
 			path: "/",
-			element: <Main />,
+			element: <Index />,
+		},
+		{
+			path: "/docs",
+			element: <Docs />,
 			errorElement: <ErrorPage />,
 			children: [
 				{
 					index: true,
-					element: <Index />,
+					element: <Intro />,
+				},
+				{
+					path: "/docs/auth",
+					element: <AuthRoutes />,
 				},
 			],
 		},
