65be396f5896408c8208897efaefc4abec79bf2f
Micah Asowata <andrewgary963@gmail.com>
2024-04-15 16:07:19 +0100

    refactor: update content of docs intro

--- src/pages/docs/Intro.jsx
+++ src/pages/docs/Intro.jsx
@@ -1,8 +1,26 @@
+import Head from "../../components/Head";
 const Intro = () => {
 	return (
-		<div>
-			<h2>Welcome to the docs</h2>
-		</div>
+		<>
+			<Head title={"Introduction"} />
+			<section>
+				<h1>Welcome to the docs</h1>
+
+				<h2>Some things to remember</h2>
+				<h3>Base URL</h3>
+				<code>https://minasa-api.micahasowata.com</code>
+				<p>
+					over the course of this documentation it would referred to as{" "}
+					<b>
+						<i>base</i>
+					</b>
+				</p>
+				<h3>Content Type</h3>
+				<code>application/json</code>
+				<h3>Authentication</h3>
+				<code>JSON Web Tokens (JWT)</code>
+			</section>
+		</>
 	);
 };
 
